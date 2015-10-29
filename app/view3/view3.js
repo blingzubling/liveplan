95c911f3dbacead5aa8d74cd89eb1164e0dcfc99
Martin Hoffmann <x@y.de>
2015-10-29 09:51:49 +0100

    rotate and translate

--- app/view3/view3.js
+++ app/view3/view3.js
@@ -15,15 +15,36 @@ angular.module('myApp.view3', ['ngRoute'])
   // var r = s.rect(100,100,100,100,20,20).attr({ stroke: '#123456', 'strokeWidth': 20, fill: 'red', 'opacity': 0.2 });
   // var t = s.text(100,50,'Snap("#svg") should reference an svg element, not a div. Or create it by supplying width,height Snap(100,100)');
 
-  var s = Snap("#blackboard"); // This will use an existing svg element (not a div)
-  var r = s.rect(10,10,100,100);
-  var c = s.circle(100,100,50);
-  r.attr('fill', 'red');     //This is the elements area to fill
-  c.attr({ 'fill': 'blue', 'stroke': 'black', 'strokeWidth': 10 });
-  r.attr({ stroke: '#123456', 'strokeWidth': 20 }); //Or this does the same thing passing one object
-  c.click( function() { this.attr('fill', 'green') } );   // using 'this'
-  r.click( function() { r.attr('fill', 'yellow') } );     
-  s.text(200,100, 'Click An Object').attr({'fill' : 'blue',  'stroke': 'blue', 'stroke-width': 0.2 });
+  var first = function(){
+    var s = Snap("#blackboard"); // This will use an existing svg element (not a div)
+
+    var c = s.circle( 200,200,10 );
+    var r = s.rect(200,100,100,100,20,20).attr({ stroke: '#123456', 'strokeWidth': 20, fill: 'red', 'opacity': 0.3 });
+
+    var rclone = r.clone();
+    var rclone2 = r.clone();
+    var rclone3 = r.clone();
+    var rclone4 = r.clone();
+    var rclone5 = r.clone();
+
+    //t=relative transform, T=absolute transform, s=relative scale, S=absolute Scale
+    //r=relative rotate, R=relative rotate
+    //relative means it takes into account previous transforms to accumulate
+    //here it doesn't make much difference, until we combine later
+
+
+    rclone.transform( 't100,100');
+    rclone2.transform( 'r20,200,200' );
+    rclone3.transform( 'r40,200,200' );
+
+    s.text(350,150,"rotate around 200,200");
+
+    rclone4.transform( 't100,100r20,200,200' );
+    rclone5.transform( 't100,100r40,200,200' );
+
+    s.text(450,250,"combined translate of 100,100 and rotate around 200,200");
+  }
+  first(); 
 
   
   var si = Snap("#svgout");
