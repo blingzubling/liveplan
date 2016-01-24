53d010cbdc6de5a49624dd91f6b279830dcd083b
unknown <hoffmann@DEF666P12.PE.local>
2016-01-24 09:43:37 +0100

    added view 5

---
+++ app/view5/view5.js
@@ -0,0 +1,28 @@
+/*global angular*/
+/*global _*/
+/*global Snap*/
+
+'use strict';
+
+angular.module('myApp.view5', ['ngRoute', 'ngResource'])
+
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/view5', {
+            redirectTo: '/plan/{D57CCB0C-9E31-44BD-9A3C-F729891B56DF}'
+        })
+        .when('/plan/:guid', {
+            templateUrl: 'view5/view5.html',
+            controller: 'View5Ctrl'
+        });
+}])
+
+.factory('gabiObject', ['$resource', function($resource) {
+    return $resource('data/:guid.json', {
+        guid: '@uuid'
+    });
+}])
+
+.controller('View5Ctrl', ['$scope', '$http', '$routeParams', '$q', 'gabiObject', function($scope, $http, $routeParams, $q, gabiObject) {
+
+}]);
