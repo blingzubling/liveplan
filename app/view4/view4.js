472ffc189e40e2b1b209e016a092f8f78194e283
hoffmann <m.hoffmann@pe-international.com>
2016-04-02 22:58:59 +0200

    tabs for parameters section

--- app/view4/view4.js
+++ app/view4/view4.js
@@ -45,6 +45,18 @@ angular.module('myApp.view4', ['ngRoute', 'ngResource', 'ngSanitize', 'smart-tab
 
         $scope.gemeinsam = gemeinsamService;
 
+        $scope.parametersTab = (function() {
+            var selected = 'tbHide'; // tbShowAll
+            return {
+                select: function(tabName) {
+                    selected = tabName;
+                },
+                isSelected: function(tabName) {
+                    return (tabName === selected);
+                }
+            };
+        })();
+
         $scope.tab = (function() {
             var selected = 'tbLCA';
             return {
