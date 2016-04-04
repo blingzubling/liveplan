54a645d02e1b77f0a59c9db5054cb69df5e6ad62
hoffmann <m.hoffmann@pe-international.com>
2016-04-04 06:58:11 +0200

    removed experimental code from plan view

--- app/view3/view3.js
+++ app/view3/view3.js
@@ -23,51 +23,6 @@ angular.module('myApp.view3', [
 
 .controller('View3Ctrl', ['$scope', '$http', '$routeParams', '$q', 'gabiObject', function($scope, $http, $routeParams, $q, gabiObject) {
 
-    var first = function() {
-        var s = Snap('#blackboard'); // This will use an existing svg element (not a div)
-
-        s.circle(200, 200, 10);
-        var r = s.rect(200, 100, 100, 100, 20, 20).attr({
-            stroke: '#123456',
-            'strokeWidth': 20,
-            fill: 'red',
-            'opacity': 0.3
-        });
-
-        var rclone = r.clone();
-        var rclone2 = r.clone();
-        var rclone3 = r.clone();
-        var rclone4 = r.clone();
-        var rclone5 = r.clone();
-
-        //t=relative transform, T=absolute transform, s=relative scale, S=absolute Scale
-        //r=relative rotate, R=relative rotate
-        //relative means it takes into account previous transforms to accumulate
-        //here it doesn't make much difference, until we combine later
-
-
-        rclone.transform('t100,100');
-        rclone2.transform('r20,200,200');
-        rclone3.transform('r40,200,200');
-
-        s.text(350, 150, 'rotate around 200,200');
-
-        rclone4.transform('t100,100r20,200,200');
-        rclone5.transform('t100,100r40,200,200');
-
-        s.text(450, 250, 'combined translate of 100,100 and rotate around 200,200');
-    };
-    first();
-
-    var second = function() {
-        var s = Snap('#svgout');
-        // var mapSvg = 
-        Snap.load('data/map.svg', function(loadedFragment) {
-            s.append(loadedFragment);
-        });
-    };
-    second();
-
     var decimalToRGB = function(decimal) {
         var R = decimal % 256;
         var G = (decimal >> 8) % 256;
